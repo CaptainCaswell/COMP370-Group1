774812afaf23d248c4d2e7ea6d5aa3d0ad7a8ac4
Ryan Caswell <ryan.caswell@student.ufv.ca>
2026-01-28 20:10:19 -0800

    Improved basic client/server.

--- Client.java
+++ Client.java
@@ -4,29 +4,46 @@ import java.util.Scanner;
 
 public class Client {
     public static void main ( String[] args ) throws Exception {
+        boolean running = true;
+
+        if ( args.length != 1 ) {
+            System.out.println( "Incorrect arguments. Include port number and message." );
+            return;
+        }
+        
         String host = "localhost";
-        int port = 5000;
+        int port = Integer.parseInt( args[0] );
 
-        System.out.println( " ---- CLIENT ---- " );
+        Scanner user = new Scanner( System.in );
 
-        // Open connection to server
-        Socket socket = new Socket( host, port );
+        while( running ) {
+            System.out.println( "What do you want to send?" );
 
-        // Set variables for socket input and output
-        Scanner input = new Scanner( socket.getInputStream() );
-        PrintStream output = new PrintStream( socket.getOutputStream() );
+            String outgoingMessage = user.nextLine();
 
-        // Send this to the server
-        String outgoingMessage = "12345";
-        System.out.println( "Sending the following to server: " + outgoingMessage );
-        output.println( outgoingMessage );
+            System.out.println( " ---- CLIENT ---- " );
 
-        // Display response from server
-        String clientMessage = input.nextLine();
-        System.out.println( "Server response: " + clientMessage );
+            // Make connection
+            Socket socket = new Socket( host, port );
 
-        // Close things
-        input.close();
-        socket.close();
+            // Set variables for socket input and output
+            Scanner input = new Scanner( socket.getInputStream() );
+            PrintStream output = new PrintStream( socket.getOutputStream() );
+
+            // Send this to the server
+            System.out.println( "Sending the following to server: " + outgoingMessage );
+            output.println( outgoingMessage );
+
+            // Display response from server
+            String clientMessage = input.nextLine();
+            System.out.println( "Server response: " + clientMessage );
+
+            // Close things
+            input.close();
+            socket.close();
+            
+        }
+
+        user.close();
     }
 }
--- ServerProcess.java
+++ ServerProcess.java
@@ -5,46 +5,97 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class ServerProcess {
-    private ServerSocket serverSocket;
-    private boolean running = true;
+    protected ServerSocket serverSocket;
+    protected boolean running = true;
+    protected static int nextID = 1;
+    protected int sum = 0;
+    protected boolean isPrimary;
+    protected int serverID;
+    protected int port;
 
-    public void start ( int port ) throws IOException {
+    // Constructor
+    public ServerProcess() {
+        this.serverID = nextID;
+        this.port = nextID + 1000;
+        nextID++;
+        this.isPrimary = !( checkPrimary() );
+    }
+
+    // Start Basic Server
+    public void start () throws IOException {
         // Open socket
         serverSocket = new ServerSocket( port );
 
         // Announce system running
-        System.out.println( " ---- SERVER ---- " );
-        System.out.println( "Server started on port " + port );
+        System.out.println( " ---- SERVER " + serverID + " ---- " );
+        System.out.println( ( isPrimary ? "Primary" : "Secondary" ) + " server started on port " + port );
 
-        
-        while ( running ) {
-            
-            // Waits for connection, returns socket when connected
-            Socket clientSocket = serverSocket.accept();
+        // What a primary server does
+        if ( isPrimary ) {
+            while ( running ) {
+                
+                // Waits for connection, returns socket when connected
+                Socket clientSocket = serverSocket.accept();
 
-            // Set variables for socket input and output
-            Scanner input = new Scanner( clientSocket.getInputStream() );
-            PrintStream output = new PrintStream( clientSocket.getOutputStream() );
+                // Set variables for socket input and output
+                Scanner input = new Scanner( clientSocket.getInputStream() );
+                PrintStream output = new PrintStream( clientSocket.getOutputStream() );
 
-            // Display input from client
-            String clientMessage = input.nextLine();
-            System.out.println( "Client sent: " + clientMessage );
+                // Display input from client
+                String clientMessage = input.nextLine();
+                System.out.println( "Client sent: " + clientMessage + ". New sum is " + sum + "." );
 
-            // Send output to client
-            String outgoingMessage = "I received the message \"" + clientMessage + "\"";
-            output.println( outgoingMessage );
+                // Process and reply
+                String outgoingMessage = processMessage( clientMessage );
+                output.println( outgoingMessage );
 
-            // Close things
-            input.close();
-            clientSocket.close();
+                // Close things
+                input.close();
+                clientSocket.close();
+            }
+        }
+
+        // What a secondary server does
+        else {
+            while( running ) {
+                System.out.println( "Waiting in standby mode..." );
+                running = false;
+            }
         }
 
         serverSocket.close();
 
     }
 
-    public static void main( String[] args ) throws Exception {
-        ServerProcess server = new ServerProcess();
-        server.start( 5000 );
+    protected String processMessage( String clientMessage ) {
+        // If message can be cast to number
+        try {
+            // Sanitize and convert message to integer
+            int number = Integer.parseInt( clientMessage.trim() );
+
+            sum += number;
+
+            return "Sum sucessful. New sum: " + sum;
+        } catch ( NumberFormatException e ) {
+            return "Sum failed. Current sum: " + sum;
+        }
     }
+
+    public void promote() {
+        isPrimary = true;
+        System.out.println( "--- System promoted ---" );
+        // Add return confirmation?
+    }
+
+    public static void main( String[] args ) throws IOException {
+        // Start server
+        ServerProcess server = new ServerProcess();
+        server.start();
+    }
+
+    protected boolean checkPrimary() {
+        // Should ask monitor is there is a primary already
+        return false;
+    }
+
 }
